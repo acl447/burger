9b912b80299ea30059d6294423cecc919e5c271d
acl <acl447@nyu.edu>
2019-05-17 16:39:03 -0700

    added routes to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,28 @@ let burger = require("../models/burger");
 let router = express.Router();
 
 
+router.get("/", function(req, res) {
 
+    burger.all(function(data) {
+
+        let hbsObject = {
+
+            burgers: data
+        };
+
+        console.log(hbsObject);
+
+        res.render("index", hbsObject);
+    })
+});
+
+router.post("/api/burgers", function(req, res) {
+
+    burger.insert(["name"], [req.body.name], function(result) {
+
+
+    })
+})
 
 
 
