761d783159c80c9e42e98930456f5771074a0390
acl <acl447@nyu.edu>
2019-05-17 14:02:04 -0700

    created code to call ORM functions using burger specific input

--- models/burger.js
+++ models/burger.js
@@ -2,14 +2,36 @@ let orm = require("../config/orm");
 
 let burger = {
 
-    all: function(cb) {
+    all: function (cb) {
 
-        orm.all("burgers")
+        orm.selectAll("burgers", function (res) {
+
+
+            cb(res);
+        })
+    },
+
+    insert: function (cb) {
+
+        orm.insertOne("burgers", cols, vals, function (res) {
+
+            cb(res);
+
+        })
+    },
+
+    update: function (cb) {
+
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+
+            cb(res);
+
+        })
     }
 
 
 
-}
+};
 
 
 
