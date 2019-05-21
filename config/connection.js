466bac27d344e0ea53c49f7c50158617a3627955
acl <acl447@nyu.edu>
2019-05-21 14:59:20 -0700

    added connection to JawsDB database

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  
+  connection = mysql.createConnection({
 
     host: "Localhost",
     port: 3306,
@@ -10,6 +14,8 @@ let connection = mysql.createConnection({
     database: "burgers_db"
 
 });
+
+};
 
 connection.connect(function(err) {
     if (err) {
