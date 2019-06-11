a3d1c3a9df57c7daef67ff2d1abfa1446fd1176c
acl <acl447@nyu.edu>
2019-06-10 17:44:53 -0700

    jawsdb schema createdAt

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE burgers (
 
     id integer auto_increment not null,
     burger_name varchar(100) not null,
+    createdAt TIMESTAMP not null,
     devoured boolean DEFAULT false,
     primary key(id)
 
